f5fc1c94413fd763621bd918c23a678344948a93
D0dii <marcindolatowski123@gmail.com>
2023-12-15 00:12:08 +0100

    first steps of adding fetching data, finishing UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import "./App.css";
+import { FormEvent, useRef } from "react";
+import useAxios from "../hooks/useAxios.tsx";
 
-function App() {
+export default function App() {
+  const inputUrlRef = useRef();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(useAxios());
+  };
+
   return (
     <>
       <div className="container">
         <h1>Paste the link you want to convert</h1>
         <section className="main-section">
-          <form className="form">
-            <input type="text" className="link-input" />
+          <form className="form" onSubmit={handleSubmit}>
+            <input
+              ref={inputUrlRef.current}
+              type="text"
+              className="link-input"
+              placeholder="Enter youtube film url"
+            />
             <button className="search-btn">Search</button>
           </form>
           <button className="download-btn">Download</button>
@@ -16,5 +29,3 @@ function App() {
     </>
   );
 }
-
-export default App;
